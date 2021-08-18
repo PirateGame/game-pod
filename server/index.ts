7722d26b48e9c9e6e4259608d81e639d096b0a36
AlexPegg <alexpeggsailing@gmail.com>
2021-08-18 21:45:25 +0100

    basic server working

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+import express, { Express } from 'express';
+import * as http from 'http';
+import next, { NextApiHandler } from 'next';
+import * as socketio from 'socket.io';
+
+const port: number = parseInt('1001');
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler: NextApiHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(async() => {
+    const app: Express = express();
+    const server: http.Server = http.createServer(app);
+    const io: socketio.Server = new socketio.Server();
+    io.attach(server);
+
+    io.on('connection', (socket: socketio.Socket) => {
+        console.log('connection');
+        socket.emit('status', 'Hello from Socket.io');
+
+        socket.on('disconnect', () => {
+            console.log('client disconnected');
+        })
+        socket.on("test", (callback: any) => {
+            callback({
+              status: "ok"
+            });
+          });
+    });
+
+    app.all('*', (req: any, res: any) => nextHandler(req, res));
+
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
